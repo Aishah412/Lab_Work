88b441e64795ab43fa634f79d958292ab90e3001
Aishah412 <carterbridgette2022@gmail.com>
2024-10-03 18:45:53 -0400

    Labwork

---
+++ computrArea.js
@@ -0,0 +1,10 @@
+function computeArea(width, height){
+    const area= (width*height) 
+return ("the area of a rectangle with a width of " + width + " and a height of " + height + " is " + area + " square units.")
+    
+   
+}
+console.log (computeArea(5,10))
+const planetHasWater = function (planet){
+    planet = planet.toLowerCase("earth" || "mars");
+}
